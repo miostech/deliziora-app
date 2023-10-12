4f77574409c7a654c77676ea509756b990651e75
Gabriel Blanco <gabrielblanco@mios.pt>
2023-10-12 16:42:58 +0100

    quadro de notificações pronto

---
+++ src/view/Notifications.js
@@ -0,0 +1,101 @@
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  TouchableWithoutFeedback,
+} from "react-native";
+import React from "react";
+import ArrowLeft from "../components/SVGs/ArrowLeft/ArrowLeft";
+
+export default function Notifications() {
+  const colors = require("../style/Colors.json");
+
+  const items = [
+    {
+      id: 1,
+      title: "Yum Tree",
+      text: "Lorem ipsum dolor sit amet leo consectetur. Mus magna sit vel leo",
+      date: "20 de outubro de 2023 ás 10:00",
+    },
+    {
+      id: 2,
+      title: "Yum Tree",
+      text: "Lorem ipsum dolor sit amet leo consectetur. Mus magna sit vel leo",
+      date: "20 de outubro de 2023 ás 10:00",
+    },
+    {
+      id: 3,
+      title: "Yum Tree",
+      text: "Lorem ipsum dolor sit amet leo consectetur. Mus magna sit vel leo",
+      date: "20 de outubro de 2023 ás 10:00",
+    },
+  ];
+
+  return (
+    <View
+      style={{
+        backgroundColor: colors.colors.neutral01Color.neutral_08,
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          marginLeft: 16,
+          marginTop: 24,
+          marginBottom: 15,
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+      >
+        <View style={{}}>
+          <TouchableOpacity>
+            <ArrowLeft />
+          </TouchableOpacity>
+        </View>
+        <View style={{ marginLeft: "15%" }}>
+          <Text style={{ fontSize: 19 }}>Quadro de Notificações</Text>
+        </View>
+      </View>
+      <ScrollView>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {items.map((item) => (
+            <TouchableWithoutFeedback>
+              <View
+                key={item.id}
+                style={{
+                  backgroundColor: "#fff",
+                  shadowColor: "#000000",
+                  shadowOpacity: 0.4,
+                  shadowRadius: 2,
+                  shadowOffset: {
+                    height: 1,
+                    width: 1,
+                  },
+                  elevation: 5,
+                  borderRadius: 8,
+                  padding: 20,
+                  margin: 20,
+                }}
+              >
+                <Text style={{fontSize:18, marginBottom:7}}>{item.title}</Text>
+                <Text style={{fontSize:12, marginBottom:7}}>{item.date}</Text>
+                <Text style={{fontSize:16 }}>{item.text}</Text>
+              </View>
+            </TouchableWithoutFeedback>
+          ))}
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
