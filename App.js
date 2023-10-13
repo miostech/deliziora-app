579d995637ffe2a6dd863a9b8d5bcf3b0d136027
Gabriel Blanco <gabrielblanco@mios.pt>
2023-10-13 13:14:05 +0100

    resolver problema de tab

--- App.js
+++ App.js
@@ -9,15 +9,125 @@ import SplashScreen from "./src/view/SplashScreen";
 import Walkthrough from "./src/view/Walkthrough";
 import Notifications from "./src/view/Notifications";
 import ArrowLeft from "./src/components/SVGs/ArrowLeft/ArrowLeft";
+import HomeScreen from "./src/view/HomeScreen";
+import NotificationSvg from "./src/components/SVGs/NotificationSvg/NotificationSvg";
+import HomeSvg from "./src/components/SVGs/HomeSvg/HomeSvg";
+import MapScreen from "./src/view/MapScreen";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const colors = require("./src/style/Colors.json");
+  function HomeTab() {
+    return (
+      <Tab.Navigator
+        initialRouteName="Home"
+        options={{ headerShadowVisible: false, headerShown: false }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerShadowVisible: false,
+            tabBarShowLabel: false,
+            headerShown: false,
+            title: "",
+            tabBarIcon: ({ focused, color, size }) => (
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <View>
+                  <HomeSvg />
+                </View>
+                <View>
+                  <Text>Inicio</Text>
+                </View>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Map"
+          component={MapScreen}
+          options={{
+            headerShadowVisible: false,
+            tabBarShowLabel: false,
+            headerShown: false,
+            title: "",
+            tabBarIcon: ({ focused, color, size }) => (
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  position: "relative",
+                }}
+              >
+                <View
+                  style={{
+                    bottom: 24,
+                    backgroundColor: "white",
+                    borderRadius: 100,
+                    width: 60,
+                    height: 60,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    borderWidth: 3,
+                    borderColor: focused ? colors.colors.baseColor.base_01 : colors.colors.neutral01Color.neutral_08,
+                  }}
+                >
+                  <HomeSvg />
+                </View>
+                <View>
+                  <Text style={{ bottom: 17 }}>Mapa</Text>
+                </View>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Notifications"
+          component={Notifications}
+          screenoptions={{ headerShadowVisible: false, animation: "fade" }}
+          options={{
+            tabBarShowLabel: false,
+            headerShadowVisible: true,
+            headerShown: false,
+            headerTitle: "",
+            tabBarIcon: ({ focused, color, size }) => (
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <View>
+                  <NotificationSvg />
+                </View>
+                <View>
+                  <Text>Notificações</Text>
+                </View>
+              </View>
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
+
   function StackApp() {
     return (
       <Stack.Navigator
-        initialRouteName="Notifications"
+        initialRouteName="HomeTab"
         screenptions={{
           headerShadowVisible: false,
           animation: "timing",
@@ -30,15 +140,12 @@ export default function App() {
           options={{ headerShadowVisible: false, headerShown: false }}
         />
         <Stack.Screen
-          name="Notifications"
-          component={Notifications}
+          name="HomeTab"
+          component={HomeTab}
           screenoptions={{ headerShadowVisible: false, animation: "fade" }}
-          options={{
-            headerShadowVisible: true,
-            headerShown: true,
-            headerTitle:"",
-          }}
+          options={{ headerShadowVisible: false, headerShown: false }}
         />
+
         <Stack.Screen
           name="Walkthrough"
           component={Walkthrough}
